6047952c26ab8412e69b81c233f14a8c2993f018
Phạm Đức Phúc <87574901+Eissen23@users.noreply.github.com>
2025-05-07 19:22:03 +0700

    modify sub_article

--- codelearner_server/routes/api/content/sub_article.php
+++ codelearner_server/routes/api/content/sub_article.php
@@ -2,6 +2,6 @@
 use App\Http\Controllers\Content\SubArticleController;
 
 Route::controller(SubArticleController::class)->group(function(){
-    Route::update('/{sub_article}', 'update');
+    Route::put('/{sub_article}', 'update');
     Route::delete('/{sub_article}', 'destroy');
 });
